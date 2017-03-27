5ae562a9c45684d356c521d961c4e4c92ce3bf07
Giampaolo Rodola <g.rodola@gmail.com>
2017-03-27 15:09:28 +0200

    add color utils

--- psutilcli/__init__.py
+++ psutilcli/__init__.py
@@ -1,3 +1,7 @@
+import sys
+import functools
+
+
 def bytes2human(n):
     # http://code.activestate.com/recipes/578019
     # >>> bytes2human(10000)
@@ -13,3 +17,77 @@ def bytes2human(n):
             value = float(n) / prefix[s]
             return '%.1f%s' % (value, s)
     return "%sB" % n
+
+
+def memoize(fun):
+    """A simple memoize decorator for functions supporting (hashable)
+    positional arguments.
+    It also provides a cache_clear() function for clearing the cache:
+
+    >>> @memoize
+    ... def foo()
+    ...     return 1
+        ...
+    >>> foo()
+    1
+    >>> foo.cache_clear()
+    >>>
+    """
+    @functools.wraps(fun)
+    def wrapper(*args, **kwargs):
+        key = (args, frozenset(sorted(kwargs.items())))
+        try:
+            return cache[key]
+        except KeyError:
+            ret = cache[key] = fun(*args, **kwargs)
+            return ret
+
+    def cache_clear():
+        """Clear cache."""
+        cache.clear()
+
+    cache = {}
+    wrapper.cache_clear = cache_clear
+    return wrapper
+
+
+@memoize
+def term_supports_colors(file=sys.stdout):
+    try:
+        import curses
+        assert file.isatty()
+        curses.setupterm()
+        assert curses.tigetnum("colors") > 0
+    except Exception:
+        return False
+    else:
+        return True
+
+
+def colorstr(s, color=None, bold=False):
+    """Return a coloured version of 'string'."""
+    # http://misc.flogisoft.com/bash/tip_colors_and_formatting
+    if not term_supports_colors():
+        return s
+
+    attr = []
+
+    if color is None:  # no color
+        pass
+    elif color == "red":
+        attr.append('31')
+    elif color == "green":
+        attr.append('32')
+    elif color == "yellow":
+        attr.append('33')
+    elif color == "blue":
+        attr.append('34')
+    elif color == "grey":
+        attr.append('90')
+    else:
+        raise ValueError("invalid color %r" % color)
+
+    if bold:
+        attr.append('1')
+
+    return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), s)
--- psutilcli/sysmem.py
+++ psutilcli/sysmem.py
@@ -15,6 +15,7 @@ from docopt import docopt
 import psutil
 
 from psutilcli import bytes2human
+from psutilcli import colorstr
 
 
 BYTES = False
@@ -40,6 +41,23 @@ def pprint_ntuple_parsable(nt, prefix):
         print('%s.%s: %s' % (prefix, name, value))
 
 
+def get_percent_grid(perc, length=40):
+    dashes = "|" * int((float(perc) / 10 * (length / 10)))
+    tot_dashes = len(dashes)
+    empty_dashes = " " * (length - tot_dashes)
+    dashes = colorstr(
+        dashes, "green" if perc <= 50 else "yellow" if perc <= 75 else "red")
+    perc = colorstr(
+        str(perc) + "%",
+        "green" if perc <= 50 else "yellow" if perc <= 75 else "red")
+    return "%s%s%s %14s%s" % (
+        colorstr("[", bold=True),
+        dashes,
+        empty_dashes,
+        perc,
+        colorstr("]", bold=True))
+
+
 def main():
     global BYTES
     args = docopt(__doc__)
@@ -51,13 +69,17 @@ def main():
         pprint_ntuple_parsable(virtual, "virtual")
         pprint_ntuple_parsable(virtual, "swap")
     else:
-        print('VIRTUAL')
-        print('-------')
+        print('%s   %s' % (
+            colorstr("Virtual", bold=1),
+            get_percent_grid(virtual.percent)))
+        print(colorstr('-' * 57, bold=1))
         pprint_ntuple(virtual)
 
         print()
-        print('SWAP')
-        print('----')
+        print('%s      %s' % (
+            colorstr("Swap", bold=1),
+            get_percent_grid(swap.percent)))
+        print(colorstr('-' * 58, bold=1))
         pprint_ntuple(swap)
 
 
